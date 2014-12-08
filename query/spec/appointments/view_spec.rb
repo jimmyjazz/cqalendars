1322a4897fa77d106a42ef677aaee19068569f0f
James Ross <james.ross@envato.com>
2014-12-08 17:47:38 +1100

    create bookmark breaking test for the appointments view

---
+++ query/spec/appointments/view_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe CQalendaRS::Query::Appointments::View do
+  
+  describe ".all" do
+    before do
+      allow(CQalendaRS::Query::Appointments::Model).to receive(:all).and_return(appointments)
+    end
+
+    it "delegates to the appointments view model" do
+      CQalendaRS::Query::Appointments::View.all
+      expect(CQalendaRS::Query::Appointments::Model).to have_received(:all)
+    end
+  end
+  
+end

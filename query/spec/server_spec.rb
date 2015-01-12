61c3036d5d75af8ffb3afbb5434f44af9dcb5b86
James Ross <james.ross@envato.com>
2015-01-12 17:47:29 +1100

    Add pending test for next session memory jogger

--- query/spec/server_spec.rb
+++ query/spec/server_spec.rb
@@ -29,9 +29,9 @@ describe "viewing my calendar" do
       expect(CQalendaRS::Query::Appointments::Model).to have_received(:all)
     end
 
-    it "passes appointments to the appointments view" do
+    pending "passes appointments to the appointments view" do
       get "/appointments"
-      expect(CQalendaRS::Query::Appointments::View).to have_received(:new).with(appointments)
+      expect(CQalendaRS::Query::Appointments::View).to have_received(:decorate).with(appointments)
     end
 
     context "I have no appointments" do

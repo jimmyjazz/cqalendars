84b8710f3fd09b69f7ccf9f18bf3ea2e4db23e38
James Ross <james.ross@envato.com>
2015-02-09 17:45:51 +1100

    Add bookmark breaking test for next session

--- query/lib/appointments/model.rb
+++ query/lib/appointments/model.rb
@@ -2,6 +2,12 @@ module CQalendaRS
   module Query
     module Appointments
       class Model
+        def self.all
+          dataset = DB[:appointments]
+          dataset.order(:start_time).map do |record|
+            Appointment.new(record.id, record.start_time, record.end_time, record.description)
+          end
+        end
       end
     end
   end
--- query/spec/appointments/model_spec.rb
+++ query/spec/appointments/model_spec.rb
@@ -4,6 +4,10 @@ describe CQalendaRS::Query::Appointments::Model do
 
   describe ".all" do
     
+    it "returns all appointments in an array of value objects" do
+      expect(CQalendaRS::Query::Appointments::Model.all).to eql([])
+    end
+    
   end
 
 end

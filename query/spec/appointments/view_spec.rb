66c0b8f121b99894698f666b6399e3228d4df2ff
James Ross <james.ross@envato.com>
2015-02-23 17:48:05 +1100

    Add bookmark broken test for next session

--- query/spec/appointments/view_spec.rb
+++ query/spec/appointments/view_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe CQalendaRS::Query::Appointments::View do
-   
+
+  describe ".decorate" do
+
+    let(:appointment) { CQalendaRS::Query::Appointments::Model::Appointment.new(123, Time.now, Time.now, "blah blah") }
+
+    it "adds a hyperlink to cancel the appointment" do
+      expect(CQalendaRS::Query::Appointments::View.decorate([appointment]).first).to include(cancel_url: "yadayada")
+    end
+
+  end
+
 end

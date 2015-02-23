2096f2cc4ecef63e873b52d3bbd2fe5057fd0fdf
James Ross <james.ross@envato.com>
2015-02-23 16:28:46 +1100

    Introduce DTO for appointments

--- query/lib/appointments/model.rb
+++ query/lib/appointments/model.rb
@@ -2,10 +2,13 @@ module CQalendaRS
   module Query
     module Appointments
       class Model
+        class Appointment < Struct.new(:id, :start_time, :end_time, :description)
+        end
+
         def self.all
           dataset = DB[:appointments]
           dataset.order(:start_time).map do |record|
-            Appointment.new(record.id, record.start_time, record.end_time, record.description)
+            Appointment.new(record[:id], record[:start_time], record[:end_time], record[:description])
           end
         end
       end
--- query/lib/db/setup.rb
+++ query/lib/db/setup.rb
@@ -6,9 +6,3 @@ DB.create_table :appointments do
   index :start_time
   index :end_time 
 end
-
-appointments = DB[:appointments]
-
-appointments.insert(start_time: "2015-02-05 09:50:00", end_time: "2015-02-05 10:20:00", description: "Collis' RubyConf Keynote")
-appointments.insert(start_time: "2015-02-04 19:00:00", end_time: "2015-02-04 23:00:00", description: "RubyConf Opening Partay!")
-appointments.insert(start_time: "2015-02-07 16:30:00", end_time: "2015-02-07 20:45:00", description: "RubyConf Picnic")

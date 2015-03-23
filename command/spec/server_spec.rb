0e9b93e84fac4dd02fd4a75866c81783d8adc463
James Ross <james.ross@envato.com>
2015-03-23 17:43:10 +1100

    Add bookmark breaking test for next time

--- command/spec/server_spec.rb
+++ command/spec/server_spec.rb
@@ -17,24 +17,25 @@ describe "adding an appointment" do
 
   describe "POST /appointments/add" do
     describe "request validation" do
-
       before do
         post "/appointments/add", payload
       end
 
-      context "with an empty payload" do
+      xit "invokes the request validator" do
+        # we imagine an object that sanity checks the request payload
+        # and at the very least ensures we have the right parameters, namely
+        # client supplied UUID for the appointment
+        # start time in UTC
+        # end time in UTC
+        # non null description
+      end
 
-        let(:payload) { {} }
-
-        it "returns a 400 Bad Request status code" do
-          expect(last_response.status).to eql(400)
-        end
-
+      xit "returns a 400 response if the validator rejects the payload" do
       end
     end
 
-    xit "invokes the correct command handler" do
-
+    it "invokes the correct command handler" do
+      expect(CQalendaRS::Command::AddAppointmentHandler).to receive(:execute)
     end
 
     xit "returns the correct response" do
